742c0465663df82efe09258eef0ea50d75cd7e07
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2026-02-01 12:20:47 +0800

    Flexible clock

--- fk723m1-zgt6/init.c
+++ fk723m1-zgt6/init.c
@@ -8,95 +8,60 @@ void Error_Handler(void) {
 }
 
 // HSE is 25 MHz
-// M = 1
-// N = 22
+// PLL in is 12.5 MHz
+static void system_clock_target(double mhz) {
+	double inte = (int)(mhz / 12.5);
+	double frac = mhz / 12.5 - inte;
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL
-  *            SYSCLK(Hz)                     = 550000000 (CPU Clock)
-  *            HCLK(Hz)                       = 275000000 (AXI and AHBs Clock)
-  *            AHB Prescaler                  = 2
-  *            D1 APB3 Prescaler              = 2 (APB3 Clock  137.5MHz)
-  *            D2 APB1 Prescaler              = 2 (APB1 Clock  137.5MHz)
-  *            D2 APB2 Prescaler              = 2 (APB2 Clock  137.5MHz)
-  *            D3 APB4 Prescaler              = 2 (APB4 Clock  137.5MHz)
-  *            HSE Frequency(Hz)              = 25000000
-  *            PLL_M                          = 1
-  *            PLL_N                          = 22
-  *            PLL_P                          = 1
-  *            PLL_Q                          = 4
-  *            PLL_R                          = 2
-  *            VDD(V)                         = 3.3
-  *            Flash Latency(WS)              = 3
-  * @param  None
-  * @retval None
-  */
-static void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  HAL_StatusTypeDef ret = HAL_OK;
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+	while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
 
-  /* The voltage scaling allows optimizing the power consumption when the device is
-     clocked below the maximum system frequency, to update the voltage scaling value
-     regarding system frequency refer to product datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+	RCC_OscInitTypeDef RCC_OscInitStruct = (RCC_OscInitTypeDef){
+		.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48,
+		.HSEState = RCC_HSE_ON,
+		.HSIState = RCC_HSI_ON, // HSI must be on of HSI48
+		.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT,
+		.HSI48State = RCC_HSI48_ON, // HSI48 used for USB
+		.CSIState = RCC_CSI_OFF,
+		.PLL = {
+			.PLLState = RCC_PLL_ON,
+			.PLLSource = RCC_PLLSOURCE_HSE,
+			.PLLM = 2,
+			.PLLN = inte,
+			.PLLFRACN = frac,
+			.PLLP = 1,
+			.PLLR = 2,
+			.PLLQ = 4,
+			.PLLVCOSEL = RCC_PLL1VCOWIDE,
+			.PLLRGE = RCC_PLL1VCIRANGE_3
+		}
+	};
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+	if (HAL_OK != HAL_RCC_OscConfig(&RCC_OscInitStruct)) { while(1); }
 
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI48;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = (RCC_ClkInitTypeDef){
+		.ClockType = 	RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | \
+				RCC_CLOCKTYPE_PCLK2  | RCC_CLOCKTYPE_D3PCLK1,
 
-  RCC_OscInitStruct.PLL.PLLM = 5;
-  RCC_OscInitStruct.PLL.PLLN = 110;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  RCC_OscInitStruct.PLL.PLLP = 1;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
+		.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+		.SYSCLKDivider = RCC_SYSCLK_DIV1,
+		.AHBCLKDivider = RCC_HCLK_DIV2,
+		.APB3CLKDivider = RCC_APB3_DIV2,
+		.APB1CLKDivider = RCC_APB1_DIV2,
+		.APB2CLKDivider = RCC_APB2_DIV2,
+		.APB4CLKDivider = RCC_APB4_DIV2
+	};
 
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
-  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  if(ret != HAL_OK)
-  {
-    /* Initialization Error */
-    while(1);
-  }
+	if (HAL_OK != HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3)) { while(1); }
 
-/* Select PLL as system clock source and configure  bus clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | \
-                                 RCC_CLOCKTYPE_PCLK2  | RCC_CLOCKTYPE_D3PCLK1);
+	// tinyusb/hw/bsp/stm32h7/boards/stm32h723nucleo/board.h
+	// We cannot easily get 48 MHz from PLL so we use HSI48
+	RCC_PeriphCLKInitTypeDef RCC_PeriphCLKInitStruct = (RCC_PeriphCLKInitTypeDef){
+		.PeriphClockSelection = RCC_PERIPHCLK_USB,
+		.UsbClockSelection = RCC_USBCLKSOURCE_HSI48
+	};
 
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
-  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
-  if(ret != HAL_OK)
-  {
-    /* Initialization Error */
-    while(1);
-  }
-
-  // tinyusb/hw/bsp/stm32h7/boards/stm32h723nucleo/board.h
-  // We cannot easily get 48 MHz from PLL so we use HSI48
-  RCC_PeriphCLKInitTypeDef RCC_PeriphCLKInitStruct = { 0 };
-
-  RCC_PeriphCLKInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  RCC_PeriphCLKInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
-  HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphCLKInitStruct);
+	if (HAL_OK != HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphCLKInitStruct)) { while(1); }
 }
 
 static void CPU_CACHE_Enable(void) {
@@ -114,5 +79,6 @@ void HAL_SYSTICK_Callback(void) {
 void system_init() {
 	//CPU_CACHE_Enable();
 	HAL_Init();
-	SystemClock_Config();
+
+	system_clock_target(408);
 }
