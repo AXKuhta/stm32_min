25edcdf053a6c2af285e475aa54f2de398754302
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2026-08-03 22:36:04 +0800

    OV2640 Camera module WIP

--- nucleo-h753zi/main.c
+++ nucleo-h753zi/main.c
@@ -1,11 +1,24 @@
 #include "stm32h7xx_hal.h"
 
 void init_clock(void);
+void init_ov2640();
 
 int main() {
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+
+	HAL_GPIO_Init(GPIOB, &(GPIO_InitTypeDef){ .Pin = GPIO_PIN_0, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FAST });
+	HAL_GPIO_Init(GPIOE, &(GPIO_InitTypeDef){ .Pin = GPIO_PIN_1, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FAST });
+	HAL_GPIO_Init(GPIOB, &(GPIO_InitTypeDef){ .Pin = GPIO_PIN_14, .Mode = GPIO_MODE_OUTPUT_PP, .Pull = GPIO_NOPULL, .Speed = GPIO_SPEED_FAST });
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, 1);
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1, 1);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
+
 	HAL_Init();
 
 	init_clock();
+
+	init_ov2640();
 
 	while (1) {};
 }
